2c7cccb7f2a10ffc92fa1f0d2233493138d0c46b
ELSHAFIE Ahmed OBS/OINIS <rtwz3803@orange.com>
2024-03-27 12:18:06 +0200

    Monitor GNB

--- internal/control_test_engine/ue/nas/service/service.go
+++ internal/control_test_engine/ue/nas/service/service.go
@@ -71,12 +71,7 @@ func UeListen(ue *context.UEContext, ueRegistrationSignal chan int, ueTerminatio
 			ueTerminationSignal <- 1
 			return
 		}
-		if n == 0 {
-			log.Warn("Unix connection closed")
-			log.Warn("Terminating UE")
-			ueTerminationSignal <- 1
-			return
-		}
+		log.Info("Received message on UE = ", ue.GetMsin())
 
 		forwardData := make([]byte, n)
 		copy(forwardData, buf[:n])
